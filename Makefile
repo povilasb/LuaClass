c576ff1238a6d119b16223c125cebab985bb6332
Povilas Balciunas <povilasb@neurotechnology.com>
2013-08-21 15:50:54 +0300

    Adds sample for class constructor.

--- Makefile
+++ Makefile
@@ -7,10 +7,15 @@ help:
 	@echo "Possible rules:"
 	@echo "\thelp - prints this help."
 	@echo "\tBasicSample - executes basic sample."
+	@echo "\tConstructor - executes sample that demonstrates constructor \
+use."
 	@echo ""
 
 BasicSample:
 	$(LUA) samples/$@.lua
 
+Constructor:
+	$(LUA) samples/$@.lua
 
-.PHONY: all BasicSample
+
+.PHONY: all BasicSample Constructor
---
+++ samples/Constructor.lua
@@ -0,0 +1,21 @@
+---
+-- This sample demonstrates the basic usage of LuaClass library.
+---
+
+-- Append LuaClass source path so that require could locate it.
+package.path = package.path .. ";src/?.lua"
+
+local LuaClass = require "LuaClass"
+
+local ClassA = LuaClass:create()
+
+function ClassA:_init(name, param1)
+	self.name = name
+	self.param1 = param1
+	print("Constructor of ClassA")
+end
+
+local objA = ClassA:new("ClassA", 10)
+
+print("Class name: " .. objA.name)
+print("Object field: ", objA.param1);
